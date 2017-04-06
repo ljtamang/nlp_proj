1c92e9cbaddb76bfaaf21c7c3b9fc3e48b4e3ed2
Lasang Jimba Tamang (ljtamang) <ljtamang@memphis.edu>
2017-04-05 19:29:38 -0500

    created evalution file

--- main.py
+++ main.py
@@ -30,16 +30,17 @@ def gold_to_discrete(source_path, desitnation_path):
     df.to_csv(desitnation_path, index=False)
 
 
-def chi_sq_feature_select(data_frame, no_of_features):
+def chi_sq_features_test(data_frame, no_of_features):
     array = data_frame.values  # values of data frame
     columns = data_frame.columns  # columns of data frame
     X = array[:, 0:(array.shape[1] - 1)]
     Y = array[:, (array.shape[1] - 1)]
     selector = SelectKBest(score_func=chi2, k=no_of_features)
     selector.fit(X, Y)
+    is_selected = selector.get_support()
     selected_features = [columns[x] for x in selector.get_support(indices=True)]
 
-    return selected_features
+    return selected_features, is_selected
 
 pcoef_scorer = make_scorer(pcorrcoef, greater_is_better=True)
 
@@ -50,7 +51,10 @@ def main():
     kfold = 10  # kfold size
 
     # Path of data set 01
-    dataset_01_path = root_dir_path() + "/data/dataset_01.csv"
+    output_path = root_dir_path() + "/result/a.csv"
+
+    # Path of data set 01
+    dataset_01_path = root_dir_path() + "/data/selected-features-file-sts-2017-training.csv"
 
     # Form data set 02
     # Convert gold value(continuous value) to  discrete value and negative values to zero.
@@ -64,39 +68,60 @@ def main():
     # Best Features Selection
     # Each CMi and RMi model will be evaluated using 10 - fold cross validation.
     # The feature set Fi will be selected as best feature if average accuracy of CMi and RMI is best.
-    best_feature = []
+    outputs = dict()
     best_avg_accuracy = 0.0
-    for i in range(1,len(dataset_01.columns)):
-    #for i in range(1, 3):
+
+    values = []  # collection of row values
+
+    # for i in range(1,len(dataset_01.columns)):
+    for i in range(1, 3):
+
         # find best i number of features
-        Fi = chi_sq_feature_select(dataset_02,i)
+        Fi = chi_sq_features_test(dataset_02,i)[0]  # selected features
+        is_selected = chi_sq_features_test(dataset_02, i)[1]  # True if selected, else false
 
         # build Gradient Boosting Regression Model, RMi
-        params = {'n_estimators': 500, 'max_depth': 3, 'min_samples_split': 2,
-                  'learning_rate': 0.01, 'loss': 'ls'}
-        RMi = ensemble.GradientBoostingRegressor(**params)
+        params = {'n_estimators': 1000, 'max_depth': 3, 'min_samples_split': 2, 'learning_rate': 0.01, 'loss': 'ls'}
+        clf = ensemble.GradientBoostingRegressor(**params)
+        GBRMi = ensemble.GradientBoostingRegressor(**params)
 
         # build Support Vector Classification model, CMi
-        CMi = svm.SVC(kernel='linear')
+        # CMi = svm.SVC(kernel='linear')
 
-        # Evaluate regression model
+        # Evaluate GBR model
         data = dataset_01[Fi].values[:, :]
         target = dataset_01.values[:, (dataset_01.values.shape[1] - 1)]
-        results = cross_val_score(RMi, data, target, cv=kfold, scoring=pcoef_scorer)
-        folds_avg_accuracy_r = np.mean(results)
+        scores = cross_val_score(GBRMi, data, target, cv=kfold, scoring=pcoef_scorer)
+        folds_avg_accuracy_r = np.mean(scores)
+        # print(folds_avg_accuracy_r)
 
+        '''
         # Evaluate classification model
         target = dataset_02.values[:, (dataset_02.values.shape[1] - 1)]  # gold values
-        results = cross_val_score(CMi, data, target, cv=kfold, scoring=pcoef_scorer)
-        folds_avg_accuracy_c = np.mean(results)
+        scores = cross_val_score(CMi, data, target, cv=kfold, scoring=pcoef_scorer)
+        folds_avg_accuracy_c = np.mean(scores)
 
         # Average accuracy of regression and classification
         folds_avg_accuracy = np.mean([folds_avg_accuracy_r,folds_avg_accuracy_c ])
+        '''
 
         # update best accuracy and features
-        if folds_avg_accuracy> best_avg_accuracy:
-            best_avg_accuracy = folds_avg_accuracy
+        if folds_avg_accuracy_r> best_avg_accuracy:
+            best_avg_accuracy = folds_avg_accuracy_r
             best_feature = Fi
+
+        # store row values
+        row_value = [i, folds_avg_accuracy_r]
+        row_value.extend(map(lambda x: 1 if x else 0, is_selected)) # uncomment if you want to add 0 and 1
+        # row_value.extend(is_selected)
+        values.append(row_value)
+        print(row_value)
+        print(len(row_value))
+
+    col_names = ["no_features_selected", str(kfold)+"_fold_avg_accuracy"]
+    col_names.extend(dataset_01.columns[:-1]) # exclude last gold column
+    df = pd.DataFrame(data=values, index=None, columns=col_names)
+    df.to_csv(path_or_buf=output_path, index=None)
 
 
 main()
